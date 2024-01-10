4059f4295dc7c99f57c1488355e623557df3ab56
morozov-ivan-sergeevich <156212847+morozov-ivan-sergeevich@users.noreply.github.com>
2024-01-10 17:54:53 +0300

    Add files via upload

---
+++ Zan1.R
@@ -0,0 +1,37 @@
+getwd()
+library(tidyverse)
+library(rnoaa)
+station_data = ghcnd_stations()
+
+Tambov = data.frame(id = "TAMBOV", latitude = 52.7317,  longitude = 41.4433)
+Tambov_around = meteo_nearby_stations(lat_lon_df = Tambov, station_data = station_data,
+                                    limit = 10, var = c("PRCP", "TAVG"),
+                                    year_min = 2006, year_max = 2006)
+Tambov_id = Tambov_around[["TAMBOV"]][["id"]]
+all_Tambov_data = meteo_tidy_ghcnd(stationid = Tambov_id, var=c("TAVG"),
+                                   date_min = "2006-01-01", date_max = "2006-12-31")
+all_Tambov_data = all_Tambov_data %>% mutate(tavg = tavg / 10)
+all_Tambov_data = all_Tambov_data %>% filter(tavg > 5)
+all_Tambov_data = all_Tambov_data %>% filter(tavg < 25)
+
+all_Tambov_data <- all_Tambov_data %>% mutate(year = year(date), month = month(date))
+all_Tambov_data <- all_Tambov_data %>% group_by(id, year, month) %>% summarise(sum_month = mean(tavg, na.rm = TRUE))
+
+var_table <- data.frame(
+  m = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12),
+  afi = c(0.00, 0.00, 0.00, 32.11, 26.31, 25.64, 23.20, 18.73, 16.30, 13.83, 0.00, 0.00),
+  bfi = c(0.00, 0.00, 0.00, 11.30, 9.26, 9.03, 8.16, 6.59, 5.73, 4.87, 0.00, 0.00),
+  di = c(0.00, 0.00, 0.00, 0.33, 1.00, 1.00, 1.00, 0.32, 0.00, 0.00, 0.00, 0.00)
+)
+q = 1600
+l = 2.2
+e = 25
+k = 300
+
+all_Tambov_data <- all_Tambov_data %>% 
+  mutate(yj = ((var_table[["afi"]][month] + var_table[["bfi"]][month] * 1.0 * sum_month) * var_table[["di"]][month] * k)/(q * l * (100 - e)))
+
+all_Tambov_data <- all_Tambov_data %>% group_by(id, year) %>% summarise(sum_year = mean(yj, na.rm = TRUE))
+all_Tambov_data <- all_Tambov_data %>% group_by(id) %>% summarise(sum_station = mean(sum_year, na.rm = TRUE))
+Y <- sum(all_Tambov_data$sum_station)
+print(paste(Y, "тонн")) 
